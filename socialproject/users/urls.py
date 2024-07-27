96124e8ef05aff0f70347598bbe5a8b959b4ad50
Avanti <avanti.a.haval@gmail.com>
2024-07-27 20:22:48 +0530

    social media app day3

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_view
 from re import template
 
 urlpatterns = [
+   path('',views.index, name='index'),
    path('login/',views.user_login,name='login'),
    path('logout/',auth_view.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+   path('password_change/',auth_view.PasswordChangeView.as_view(),name='password_change'),
 ]
